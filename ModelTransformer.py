a05d9698a8147eaf2eaaa3d56f3a6d8f9bba110b
Nicolas Anctil <anctil.nicolas.b@gmail.com>
2017-04-23 12:48:10 -0400

    Tests to find lines in the field to help homography

--- ModelTransformer.py
+++ ModelTransformer.py
@@ -14,6 +14,7 @@ class ModelTransformer:
         self.rows, self.cols, channels = first_frame.shape
         self.model = cv2.resize(model.image, (self.cols, self.rows))
 
+        cv2.imshow('frame_hsv', self.line_mask(first_frame))
         cv2.imshow('frame', first_frame)
         cv2.setMouseCallback("frame", self.click_gather_point)
         cv2.waitKey()
@@ -26,19 +27,8 @@ class ModelTransformer:
         cv2.waitKey()
         cv2.destroyAllWindows()
         model_pts = self.refPT
-        self.refPT = list()
-
-        print(frame_pts)
-        print(model_pts)
 
         self.H = cv2.getPerspectiveTransform(np.float32(frame_pts), np.float32(model_pts))
-        print(self.H)
-
-        frame_transformed = cv2.warpPerspective(first_frame, self.H, (self.cols, self.rows))
-
-        cv2.imshow('model', cv2.addWeighted(self.model, 1, frame_transformed, 1, 0))
-        cv2.waitKey()
-        cv2.destroyAllWindows()
 
     def click_gather_point(self, event, x, y, flags, param):
         if event == cv2.EVENT_LBUTTONDOWN:
@@ -47,6 +37,20 @@ class ModelTransformer:
             self.refPT.append(point)
 
     def new_frame(self, frame):
+        M = self.get_homography_between_frames(frame)
+        tmp_H = np.dot(M, self.H)
+        lines = self.find_lines(frame)
+
+        while not self.are_lines_vertical_in_model(lines, tmp_H):
+            M = self.get_homography_between_frames(frame)
+            tmp_H = np.dot(M, self.H)
+            lines = self.find_lines(frame)
+
+        self.H = tmp_H
+
+        self.last_frame = frame
+
+    def get_homography_between_frames(self, frame):
         # Initiate SIFT detector
         orb = cv2.ORB_create()
 
@@ -63,19 +67,57 @@ class ModelTransformer:
         # Sort them in the order of their distance.
         matches = sorted(matches, key=lambda x: x.distance)
 
-        src_pts = np.float32([kp1[m.queryIdx].pt for m in matches[:10]]).reshape(-1, 1, 2)
-        dst_pts = np.float32([kp2[m.trainIdx].pt for m in matches[:10]]).reshape(-1, 1, 2)
+        src_pts = np.float32([kp1[m.queryIdx].pt for m in matches[:6]]).reshape(-1, 1, 2)
+        dst_pts = np.float32([kp2[m.trainIdx].pt for m in matches[:6]]).reshape(-1, 1, 2)
 
-        #M = cv2.getPerspectiveTransform(src_pts, dst_pts)
-        M, _ = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
-        self.H = np.dot(M, self.H)
+        M, _ = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 10000)
+        return M
 
-        frame_transformed = cv2.warpPerspective(frame, self.H, (self.cols, self.rows))
+    def find_lines(self, frame):
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        mask = np.zeros(frame.shape, np.uint8)
+        edges = cv2.Canny(gray, 300, 600, apertureSize=3)
 
-        cv2.imshow('model', cv2.addWeighted(self.model, 1, frame_transformed, 1, 0))
-        cv2.waitKey()
-        cv2.destroyAllWindows()
+        return cv2.HoughLines(edges, 1, np.pi / 180, 200, 0, 0)[:, 0, :]
 
-        self.last_frame = frame
+    def are_lines_vertical_in_model(self, lines, H):
+        return True
+
+    def line_mask(self, frame):
+
+        # grass [45, 44, 89] [55, 86, 174]
+        # white [0, 0, 150] [165, 40, 255]
+        min_hsv = np.asarray([45, 44, 89])
+        max_hsv = np.asarray([55, 86, 174])
+        dst = cv2.GaussianBlur(frame, (11, 11), 0)
+        hsv = cv2.cvtColor(dst, cv2.COLOR_BGR2HSV)
+        _grass_mask = cv2.inRange(hsv, min_hsv, max_hsv)
+        grass_mask = np.zeros(frame.shape, np.uint8)
+        for i in range(frame.shape[2]):
+            grass_mask[:, :, i] = _grass_mask
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        mask = np.zeros(frame.shape, np.uint8)
+        edges = cv2.Canny(gray, 300, 600, apertureSize=3)
+
+        lines = cv2.HoughLines(edges, 1, np.pi/180, 200, 0, 0)
+        print(lines.shape)
+        for line in lines[:, 0, :]:
+            rho, theta = line
+            a = np.cos(theta)
+            b = np.sin(theta)
+            x0 = a * rho
+            y0 = b * rho
+            x1 = int(x0 + 1000 * (-b))
+            y1 = int(y0 + 1000 * (a))
+            x2 = int(x0 - 1000 * (-b))
+            y2 = int(y0 - 1000 * (a))
+
+            cv2.line(mask, (x1, y1), (x2, y2), (255, 255, 255), 4)
+
+        #mask = cv2.bitwise_or(grass_mask, mask)
+        #return cv2.bitwise_and(mask, frame)
+        return mask
+
 
 
--- main.py
+++ main.py
@@ -13,8 +13,19 @@ modelImage = cv2.imread('resources/model/model_cfl.png')
 model = Model(modelImage)
 modelTr = ModelTransformer(model, frames[13])
 
+frame_transformed = cv2.warpPerspective(frames[13], modelTr.H, (modelTr.cols, modelTr.rows))
+cv2.imshow('model', cv2.addWeighted(modelTr.model, 1, frame_transformed, 1, 0))
+cv2.waitKey()
+
 for frame in frames[14:]:
     modelTr.new_frame(frame)
+    frame_transformed = cv2.warpPerspective(frame, modelTr.H, (modelTr.cols, modelTr.rows))
+    mask_transformed = cv2.warpPerspective(modelTr.line_mask(frame), modelTr.H, (modelTr.cols, modelTr.rows))
+    cv2.imshow('model', cv2.addWeighted(modelTr.model, 1, frame_transformed, 1, 0))
+    cv2.imshow('model_mask', cv2.addWeighted(modelTr.model, 1, mask_transformed, 1, 0))
+    cv2.waitKey()
+
+cv2.destroyAllWindows()
 
 
 #for frame in frames:
